7504aaefe937b0d54e4915e7f7859d673b9ba36a
art-tours <86408659+art-tours@users.noreply.github.com>
2021-06-24 14:09:14 +0400

    commint 24/6/2021

---
+++ js/main.js
@@ -0,0 +1,16 @@
+new Swiper('.tours__item-slider-container', {
+   slidesPerView: 1,
+   autoHeight: true,
+   navigation: {
+      nextEl: '.tours__item-slider-next',
+      prevEl: '.tours__item-slider-prev',
+   },
+});
+new Swiper('.places__item-slider-container', {
+   slidesPerView: 1,
+   autoHeight: true,
+   navigation: {
+      nextEl: '.places__item-slider-next',
+      prevEl: '.places__item-slider-prev',
+   },
+});
